81a140a049e263d353e6e812b28cbf4d620cb0fb
Strulak Adrian <astrulak@pkfconsult.local>
2020-05-27 15:22:55 +0200

    initial

---
+++ mouse_clicker.py
@@ -0,0 +1,116 @@
+import time
+import threading
+from random import randrange
+from pynput.mouse import Button
+from pynput.mouse import Controller as MauseControler
+from pynput.keyboard import Controller as KeyboardControler
+from pynput.keyboard import Listener, KeyCode, Key
+
+
+delay = 20
+button = Button.left
+key_down = Key.down
+key_enter = Key.enter
+start_stop_key = KeyCode(char='s')
+exit_key = Key.esc
+
+class ClickMause(threading.Thread):
+    def __init__(self, delay, button):
+        super().__init__()
+        self.delay = delay
+        self.button = button
+        self.key_down = key_down
+        self.key_enter = key_enter
+        self.running = False
+        self.program_running = True
+    
+    def start_clicking(self):
+        self.running = True
+    
+    def stop_clicking(self):
+        self.running = False
+    
+    def exit(self):
+        self.stop_clicking()
+        self.program_running = False
+
+    def run(self):
+        while self.program_running:
+            while self.running:
+
+                randx = randrange(680, 1416)
+                randy = randrange(325, 803)
+
+                t = time.localtime()
+                cur = time.strftime('%I:%M:%S %p', t)
+
+                print("Moving mouse to pos: ", randx, randy, cur)
+
+                mouse.position = (randx, randy, t)
+                time.sleep(self.delay)
+                    # mouse.position = (2015, 241)
+                    # mouse.click(self.button)
+                    # time.sleep(self.delay)
+                    # mouse.position = (1836, 791)
+                    # mouse.click(self.button)
+                    # time.sleep(self.delay)
+
+
+                    # mouse.position = (1806, 524)
+                    # mouse.click(self.button)
+                    # time.sleep(self.delay)
+                    # keyboard.press(self.key_enter)
+                    # keyboard.release(self.key_enter)
+                    # mouse.position = (1827, 835)
+                    # mouse.click(self.button)
+                    # time.sleep(self.delay)
+                    # keyboard.press(self.key)
+                    # keyboard.release(self.key)
+                    # time.sleep(self.delay)
+
+
+                    # mouse.position = (1445, 846) # group
+                    # mouse.click(self.button)
+                    # time.sleep(self.delay)
+                    # mouse.position = (1450, 278) # select group
+                    # mouse.click(self.button)
+                    # time.sleep(self.delay)
+                    # mouse.position = (1364, 472) # rm group
+                    # mouse.click(self.button)
+                    # time.sleep(self.delay)
+                    # keyboard.press(self.key_enter) # enter
+                    # keyboard.release(self.key_enter)
+                    # mouse.position = (1384, 812) # close
+                    # mouse.click(self.button)
+                    # time.sleep(self.delay)
+                    # mouse.position = (1832, 840) # close 2
+                    # mouse.click(self.button)
+                    # time.sleep(self.delay)
+                    # keyboard.press(self.key_down) # press down
+                    # keyboard.release(self.key_down)
+                    # time.sleep(self.delay)
+                    
+
+
+mouse = MauseControler()
+keyboard = KeyboardControler()
+click_thread = ClickMause(delay, button)
+click_thread.start()
+print("")
+print("Press 's' to start script!")
+
+
+def on_press(key): 
+        if key == start_stop_key:
+            if click_thread.running:
+                click_thread.stop_clicking()
+            else:
+                click_thread.start_clicking()
+
+        elif key == exit_key:
+            print("Stoping mouse :D")
+            click_thread.exit()
+            listener.stop()
+
+with Listener(on_press=on_press) as listener:
+    listener.join()
---
+++ shortcuts.py
@@ -0,0 +1,107 @@
+﻿import time
+import re
+from pynput import keyboard
+from pynput.keyboard import Key, Controller, Listener
+
+def fix_string(s, **changes):
+    changes = dict((re.escape(k), v) for k, v in changes.items())
+    pattern = re.compile("|".join(changes.keys()))
+    return pattern.sub(lambda m: changes[re.escape(m.group(0))], sExit)
+
+def shorten_tip(message):
+    ms_len = 50
+    if len(message) > ms_len:
+        last = message[:ms_len].strip()[-1]
+        special = [",", "/"]
+        if last in special:
+            return message[:ms_len].replace(last, "").strip() + '...'
+        return message[:ms_len].strip() + '...'
+    return message
+
+kbd = Controller()
+t = 0.02
+tip = 'Use {} to type "{}"'
+s1 = "q+`"
+s2 = "q+;"
+s3 = "q+'"
+s4 = "q+/"
+s5 = "q+1"
+s6 = "q+2"
+sExit = "<Shift>+<Esc>"
+ch = {"<": " ", ">": " "}
+sExit_fixed = fix_string(sExit, **ch)
+
+t1 = "Wersja 1-2 (aktualna), Brak błędów."
+t2 = "Wersja 1-2 (aktualna), W sprawozdaniu są następujące błędy:"
+t3 = "Wersja 1-0 (nieaktualna)."
+t4 = "Cześć, timsheetami, godzinami pracy, nadgodzinami, zajmuje się kontroling, napisz na controlling@pkfpolska.pl."
+t5 = "Zaakceptuj mi logowanie, za chwilę."
+t6 = "link do połączenia."
+
+tip_text = {s1: t1, s2: t2, s3: t3, s4: t4, s5: t5, s6: t6}
+
+print('')
+print('Welcome')
+print('Use shortcuts to type things.')
+print('')
+print('=========Use {} to stop script========='.format(sExit_fixed.strip()))
+print('')
+
+for k, v in tip_text.items():
+    print(tip.format(k.replace("", " ")[1:-1], shorten_tip(v)))
+
+
+def remove_shortcut_text():
+    for i in range(2):
+        kbd.press(Key.backspace)
+        kbd.release(Key.backspace)
+
+def press_buttons(c):
+    kbd.press(c)
+    kbd.release(c)
+    time.sleep(t)
+    
+
+def activate_12_good():
+    remove_shortcut_text()
+    for c in t1:
+        press_buttons(c)
+
+def activate_12_bad():
+    remove_shortcut_text()
+    for c in t2:
+        press_buttons(c)
+
+def activate_10():
+    remove_shortcut_text()
+    for c in t3:
+        press_buttons(c)
+
+def ts_control():
+    remove_shortcut_text()
+    for c in t4:
+        press_buttons(c)
+
+def g2a():
+    remove_shortcut_text()
+    for c in t5:
+        press_buttons(c)
+
+def link_g2a():
+    remove_shortcut_text()
+    for c in t6:
+        press_buttons(c)
+
+def stop_script():
+    h.stop()
+
+
+with keyboard.GlobalHotKeys({
+        s1: activate_12_good,
+        s2: activate_12_bad,
+        s3: activate_10,
+        s4: ts_control,
+        s5: g2a,
+        s6: link_g2a,
+        sExit: stop_script}) as h:
+    h.join()
