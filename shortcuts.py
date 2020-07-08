1bce7aea48f8a331e7b82b458c23aae4152e6427
Strulak Adrian <astrulak@pkfconsult.local>
2020-07-08 12:06:58 +0200

    script to class

--- shortcuts.py
+++ shortcuts.py
@@ -6,136 +6,139 @@ from ctypes import wintypes
 from pynput import keyboard
 from pynput.keyboard import Key, Controller, Listener
 
-title = "Background Shortcuts"
-os.system('title ' + title)
-os.system('color 30')
+class ShortCuts:
+    def __init__(self):
+        super(ShortCuts, self).__init__()
+        title = "Background Shortcuts"
+        os.system('title ' + title)
+        os.system('color 30')
+        self.kbd = Controller()
+        self.t = 0.02
+        self.t_minimize = 1
+        tip = 'Use {} to type "{}"'
+        self.s1 = "q+`"
+        self.s2 = "q+;"
+        self.s3 = "q+'"
+        self.s4 = "q+/"
+        self.s5 = "q+1"
+        self.s6 = "q+2"
+        self.s7 = "q+3"
+        self.sExit = "<Shift>+<Esc>"
+        ch = {"<": " ", ">": " "}
+        sExit_fixed = self.fix_string(self.sExit, **ch)
 
-def minimize():
-    user32 = ctypes.windll.user32
-    h_wnd = user32.GetForegroundWindow()
-    user32.ShowWindow(h_wnd, 6)
-
-def fix_string(s, **changes):
-    changes = dict((re.escape(k), v) for k, v in changes.items())
-    pattern = re.compile("|".join(changes.keys()))
-    return pattern.sub(lambda m: changes[re.escape(m.group(0))], sExit)
-
-def shorten_tip(message):
-    ms_len = 50
-    if len(message) > ms_len:
-        last = message[:ms_len].strip()[-1]
-        special = [",", "/"]
-        if last in special:
-            return message[:ms_len].replace(last, "").strip() + '...'
-        return message[:ms_len].strip() + '...'
-    return message
-
-minimize()
-kbd = Controller()
-t = 0.02
-t_minimize = 1
-tip = 'Use {} to type "{}"'
-s1 = "q+`"
-s2 = "q+;"
-s3 = "q+'"
-s4 = "q+/"
-s5 = "q+1"
-s6 = "q+2"
-s7 = "q+3"
-sExit = "<Shift>+<Esc>"
-ch = {"<": " ", ">": " "}
-sExit_fixed = fix_string(sExit, **ch)
-
-t1 = "Wersja 1-2 (aktualna), Brak błędów."
-t2 = "Wersja 1-2 (aktualna), W sprawozdaniu są następujące błędy:"
-t3 = "Wersja 1-0 (nieaktualna)."
-t4 = "Cześć, timsheetami, godzinami pracy, nadgodzinami, zajmuje się kontroling, napisz na controlling@pkfpolska.pl."
-t5 = "Zaakceptuj mi logowanie, za chwilę."
-t6 = "link do połączenia."
-t7_placeholder = "Dostaliśmy proces odebrania uprawnień dla {}, podczas nadawania uprawnień została również ustawiona data ich odebrania. \
+        self.t1 = "Wersja 1-2 (aktualna), Brak błędów."
+        self.t2 = "Wersja 1-2 (aktualna), W sprawozdaniu są następujące błędy:"
+        self.t3 = "Wersja 1-0 (nieaktualna)."
+        self.t4 = "Cześć, timsheetami, godzinami pracy, nadgodzinami, zajmuje się kontroling, napisz na controlling@pkfpolska.pl."
+        self.t5 = "Zaakceptuj mi logowanie, za chwilę."
+        self.t6 = "link do połączenia."
+        self.t7_placeholder = "Dostaliśmy proces odebrania uprawnień dla {}, podczas nadawania uprawnień została również ustawiona data ich odebrania. \
 Proces generuje się automatycznie.\rCzy mam zablokować konto z dniem {}?"
 
-tip_text = {s1: t1, s2: t2, s3: t3, s4: t4, s5: t5, s6: t6, s7: t7_placeholder}
+        self.tip_text = {self.s1: self.t1, self.s2: self.t2, self.s3: self.t3, self.s4: self.t4, self.s5: self.t5, self.s6: self.t6, self.s7: self.t7_placeholder}
 
-print('')
-print('Welcome')
-print('Use shortcuts to type things.')
-print('')
-print('=========Use {} to stop script========='.format(sExit_fixed.strip()))
-print('')
+        print('')
+        print('Welcome')
+        print('Use shortcuts to type things.')
+        print('')
+        print('=========Use {} to stop script========='.format(sExit_fixed.strip()))
+        print('')
 
-for k, v in tip_text.items():
-    print(tip.format(k.replace("", " ")[1:-1], shorten_tip(v)))
+        for k, v in self.tip_text.items():
+            print(tip.format(k.replace("", " ")[1:-1], self.shorten_tip(v)))
 
-list_of = []
-for k, v in tip_text.items():
-    list_of.extend([k[0],k[-1]])
-list_of = list(set(list_of))
+        self.list_of = []
+        for k, v in self.tip_text.items():
+            self.list_of.extend([k[0],k[-1]])
+        self.list_of = list(set(self.list_of))
 
-def remove_shortcut_text():
-    for i in range(2):
-        kbd.press(Key.backspace)
-        kbd.release(Key.backspace)
-    for k in list_of:
-        kbd.release(k)
+    def minimize(self):
+        user32 = ctypes.windll.user32
+        h_wnd = user32.GetForegroundWindow()
+        user32.ShowWindow(h_wnd, 6)
 
-def press_buttons(c):
-    kbd.press(c)
-    kbd.release(c)
-    time.sleep(t)
-    
+    def fix_string(self, s, **changes):
+        changes = dict((re.escape(k), v) for k, v in changes.items())
+        pattern = re.compile("|".join(changes.keys()))
+        return pattern.sub(lambda m: changes[re.escape(m.group(0))], self.sExit)
 
-def activate_12_good():
-    remove_shortcut_text()
-    for c in t1:
-        press_buttons(c)
+    def shorten_tip(self, message):
+        ms_len = 50
+        if len(message) > ms_len:
+            last = message[:ms_len].strip()[-1]
+            special = [",", "/"]
+            if last in special:
+                return message[:ms_len].replace(last, "").strip() + '...'
+            return message[:ms_len].strip() + '...'
+        return message
 
-def activate_12_bad():
-    remove_shortcut_text()
-    for c in t2:
-        press_buttons(c)
+    def remove_shortcut_text(self):
+        for i in range(2):
+            self.kbd.press(Key.backspace)
+            self.kbd.release(Key.backspace)
+        for k in self.list_of:
+            self.kbd.release(k)
 
-def activate_10():
-    remove_shortcut_text()
-    for c in t3:
-        press_buttons(c)
+    def press_buttons(self, c):
+        self.kbd.press(c)
+        self.kbd.release(c)
+        time.sleep(self.t)
+        
 
-def ts_control():
-    remove_shortcut_text()
-    for c in t4:
-        press_buttons(c)
+    def activate_12_good(self):
+        self.remove_shortcut_text()
+        for c in self.t1:
+            self.press_buttons(c)
 
-def g2a():
-    remove_shortcut_text()
-    for c in t5:
-        press_buttons(c)
+    def activate_12_bad(self):
+        self.remove_shortcut_text()
+        for c in self.t2:
+            self.press_buttons(c)
 
-def link_g2a():
-    remove_shortcut_text()
-    for c in t6:
-        press_buttons(c)
+    def activate_10(self):
+        self.remove_shortcut_text()
+        for c in self.t3:
+            self.press_buttons(c)
 
-def odebranie_up():
-    remove_shortcut_text()
-    worker_name = input("Wprowadź nazwę Pracownika:")
-    due_date = input("Wprowadź datę wyłączena konta:")
-    t7 = t7_placeholder.format(worker_name, due_date)
-    minimize()
-    time.sleep(t_minimize)
-    for c in t7:
-        press_buttons(c)
+    def ts_control(self):
+        self.remove_shortcut_text()
+        for c in self.t4:
+            self.press_buttons(c)
 
-def stop_script():
-    h.stop()
+    def g2a(self):
+        self.remove_shortcut_text()
+        for c in self.t5:
+            self.press_buttons(c)
 
+    def link_g2a(self):
+        self.remove_shortcut_text()
+        for c in self.t6:
+            self.press_buttons(c)
 
-with keyboard.GlobalHotKeys({
-        s1: activate_12_good,
-        s2: activate_12_bad,
-        s3: activate_10,
-        s4: ts_control,
-        s5: g2a,
-        s6: link_g2a,
-        s7: odebranie_up,
-        sExit: stop_script}) as h:
-    h.join()
+    def odebranie_up(self):
+        self.remove_shortcut_text()
+        worker_name = input("Wprowadź nazwę Pracownika:")
+        due_date = input("Wprowadź datę wyłączena konta:")
+        t7 = self.t7_placeholder.format(worker_name, due_date)
+        self.minimize()
+        time.sleep(self.t_minimize)
+        for c in t7:
+            self.press_buttons(c)
+
+    def stop_script(self):
+        h.stop()
+
+if __name__ == "__main__":
+    sc = ShortCuts()
+    sc.minimize()
+    with keyboard.GlobalHotKeys({
+            sc.s1: sc.activate_12_good,
+            sc.s2: sc.activate_12_bad,
+            sc.s3: sc.activate_10,
+            sc.s4: sc.ts_control,
+            sc.s5: sc.g2a,
+            sc.s6: sc.link_g2a,
+            sc.s7: sc.odebranie_up,
+            sc.sExit: sc.stop_script}) as h:
+        h.join()
